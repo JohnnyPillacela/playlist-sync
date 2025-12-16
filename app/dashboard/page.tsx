73552271f7c97c2f447ba7a8b2d0ad05b31c4a55
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-16 14:44:02 -0500

    Display songs in cards with images and total count

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 // app/dashboard/page.tsx
 
 import { Song } from "@/lib/constants/song";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const playlistUrl = `${baseUrl}/api/spotify/playlists`;
@@ -22,11 +24,24 @@ export default async function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
-            <ul>
-                {songs.map((song: Song) => (
-                    <li key={song.name}>{song.name} - {song.artists.join(', ')}</li>
-                ))}
-            </ul>
+            <p>Total songs: {songs.length}</p>
+
+            <div className="w-3/4 mx-auto mt-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
+                    {songs.map((song: Song) => (
+                        <Card key={song.id}>
+                            <CardHeader>
+                                <CardTitle>{song.name}</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <Image src={song.trackAlbumImageUrl} alt={song.name} width={100} height={100} />
+                                <p>{song.artists.join(', ')}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </div>
+
         </div>
     )
 }
