42a86e647289f193ea32a966a581fe0aaf7aee49
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-16 14:07:59 -0500

    Fetch from local playlist api and display

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,32 @@
 // app/dashboard/page.tsx
 
-export default function Dashboard() {
+import { Song } from "@/lib/constants/song";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const playlistUrl = `${baseUrl}/api/spotify/playlists`;
+
+const fetchSongs = async () => {
+    const response = await fetch(playlistUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+export default async function Dashboard() {
+    const songs = await fetchSongs();
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <ul>
+                {songs.map((song: Song) => (
+                    <li key={song.name}>{song.name} - {song.artists.join(', ')}</li>
+                ))}
+            </ul>
         </div>
     )
 }
