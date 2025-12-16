ad7f00519693be32175baf3f9634dd77139e075e
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-16 13:21:12 -0500

    fetch playlists using access_token

--- app/api/spotify/playlists/route.ts
+++ app/api/spotify/playlists/route.ts
@@ -1,7 +1,22 @@
 // /api/spotify/playlists
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { _fetchTokenDetails } from "../auth/route";
 
+const tokenDetails = await _fetchTokenDetails();
+const worshipHimPlaylistID = "6Qrp1OAdtxJomUi1RyhyPr"
 
-export function GET(request: NextRequest) {
-    return new Response('Hello, world!');
+const _fetchPlaylistTracks = async (playlistID: string, accessToken: string) => {
+    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+export async function GET(request: NextRequest) {
+    const playlistTracks = await _fetchPlaylistTracks(worshipHimPlaylistID, tokenDetails.access_token);
+    return NextResponse.json(playlistTracks);
 }
