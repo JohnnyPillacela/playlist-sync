9bbe3797c8902d30c26c5389790da6ecc0a17342
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-16 15:05:56 -0500

    add style

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,11 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-      Welcome to the playlist sync app!
-
-      <br />
-      <Button variant="outline" onClick={() => router.push('/dashboard')}>View all playlists</Button>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <h1 className="mb-4 text-4xl font-semibold">Welcome to the playlist sync app!</h1>
+      <Button variant="outline" size="lg" onClick={() => router.push('/dashboard')}>
+        View all playlists
+      </Button>
     </div>
   );
 }
