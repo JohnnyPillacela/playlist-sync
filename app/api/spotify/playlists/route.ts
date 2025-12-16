779b11a401b10dbc6dc6ca3123838591acf6cc44
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-16 15:58:06 -0500

    Refactor Spotify playlist track fetching by removing inline function and updating imports

--- app/api/spotify/playlists/route.ts
+++ app/api/spotify/playlists/route.ts
@@ -1,21 +1,11 @@
 // /api/spotify/playlists
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { _fetchTokenDetails } from "@/lib/spotify/auth";
 import { Song } from "@/lib/constants/song";
+import { _fetchPlaylistTracks } from "@/lib/spotify/playlists";
 
 const tokenDetails = await _fetchTokenDetails();
 const worshipHimPlaylistID = "6Qrp1OAdtxJomUi1RyhyPr"
-
-const _fetchPlaylistTracks = async (playlistID: string, accessToken: string) => {
-    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-        },
-    });
-    const data = await response.json();
-    return data;
-}
 
 export async function GET() {
     const playlistTracks = await _fetchPlaylistTracks(worshipHimPlaylistID, tokenDetails.access_token);
