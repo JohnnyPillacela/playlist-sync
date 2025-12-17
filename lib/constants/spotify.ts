ba4901ba06d6b452a73545784b4d7a5a75f93139
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-17 00:13:30 -0500

    Add SpotifyUser type definition for user details

---
+++ lib/constants/spotify.ts
@@ -0,0 +1,14 @@
+
+
+export type SpotifyUser = {
+    id: string;
+    country: string;
+    display_name: string;
+    email: string;
+    external_url: string;
+    followers: string;
+    href: string;
+    product: string;
+    type: string;
+    uri: string;
+}
