d5ea3218f63c3c9506e7932de86ff6734ffb6bc4
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-17 01:03:34 -0500

    Add function to fetch user's playlists from Spotify API using cookies

--- lib/spotify/playlists.ts
+++ lib/spotify/playlists.ts
@@ -1,5 +1,8 @@
 // /lib/spotify/playlists.ts
 
+import { _getCurrentUserDetails } from "./auth";
+import { cookies } from "next/headers";
+
 export async function _fetchPlaylistTracks(playlistID: string, accessToken: string) {
     const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}/tracks`, {
         method: 'GET',
@@ -9,4 +12,42 @@ export async function _fetchPlaylistTracks(playlistID: string, accessToken: stri
     });
     const data = await response.json();
     return data;
+}
+
+export async function _fetchUsersPlaylists() {
+    const currentUser = await _getCurrentUserDetails();
+
+    if(!currentUser){
+        throw new Error('No current user found');
+    }
+
+    const cookieStore = await cookies();
+    const access_token = cookieStore.get('access_token')?.value;
+
+    if(!access_token){
+        throw new Error('No access token found');
+    }
+
+    const userId = currentUser.id;
+
+    const response = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${access_token}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch playlists for user ${currentUser.display_name}`);
+    }
+
+    const data = await response.json();
+
+    const playlistDetails: { total: number, playlists: any[] } = { total: 0, playlists: [] };
+    const playlists: any[] = data.items;
+
+    playlistDetails.total = data.total;
+    playlistDetails.playlists = playlists;
+
+    return playlistDetails;
 }
