d75e2e4a9fb610849444fb06195c68af49945d71
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-17 00:22:20 -0500

    update user type

--- lib/constants/spotify.ts
+++ lib/constants/spotify.ts
@@ -1,12 +1,12 @@
-
+// /lib/constants/spotify.ts
 
 export type SpotifyUser = {
     id: string;
     country: string;
     display_name: string;
     email: string;
-    external_url: string;
-    followers: string;
+    external_url: string; // opens spotify client
+    followers: number; // Total number of followers
     href: string;
     product: string;
     type: string;
