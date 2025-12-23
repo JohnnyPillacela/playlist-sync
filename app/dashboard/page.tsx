0b7a7440dc39d25b9db4fdbeef1d56b6224899c7
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-23 18:29:23 -0500

    Refactor Dashboard to utilize PlaylistViewer component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,17 +8,14 @@ import PleaseSignIn from "@/components/please-sign-in";
 import { SimplifiedPlaylist, Track } from "@spotify/web-api-ts-sdk";
 import PlaylistCarousel from "@/components/playlist-carousel";
 import GreetUserCard from "@/components/GreetUserCard";
+import PlaylistViewer from "@/components/playlist-viewer";
 
 export default async function Dashboard() {
-    const playlistID = "5Iavgt4CvEYZ2tJXfyqNPw"
-    
-    let playlists: SimplifiedPlaylist[] = [];
     let user: SpotifyUser | null = null;
-    let songs: Track[] = [];
+    let playlists: SimplifiedPlaylist[] = [];
     
     try {
         user = await _getCurrentUserDetails();
-        songs = await _fetchPlaylistTracksSDK(playlistID);
         playlists = await _fetchUsersPlaylists();
     } catch (error) {
         console.error('Error fetching user:', error);
@@ -37,17 +34,7 @@ export default async function Dashboard() {
                 <GreetUserCard user={user} playlists={playlists.length} />
             </div>
 
-            <div className="w-3/4 mx-auto mt-4">
-                <PlaylistCarousel playlists={playlists} />
-            </div>
-
-            <div className="w-3/4 mx-auto mt-4">
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
-                    {songs.map((track: Track) => (
-                        <SongCard key={track.id} track={track} />
-                    ))}
-                </div>
-            </div>
+            <PlaylistViewer playlists={playlists} />
 
         </div>
     )
--- components/playlist-viewer.tsx
+++ components/playlist-viewer.tsx
@@ -1,9 +1,32 @@
 // /components/playlist-viewer.tsx
 
-export default function PlaylistViewer() {
+import { SimplifiedPlaylist, Track } from "@spotify/web-api-ts-sdk";
+import PlaylistCarousel from "./playlist-carousel";
+import { _fetchPlaylistTracksSDK } from "@/lib/spotify/playlists";
+import { SongCard } from "./song-card";
+
+interface PlaylistViewerProps {
+    playlists: SimplifiedPlaylist[];
+}
+
+export default async function PlaylistViewer({ playlists }: PlaylistViewerProps) {
+    const playlistID = "5Iavgt4CvEYZ2tJXfyqNPw"
+    const songs = await _fetchPlaylistTracksSDK(playlistID);
+
     return (
-        <div>
-            <h1>Playlist Viewer</h1>
-        </div>
+
+        <>
+            <div className="w-3/4 mx-auto mt-4">
+                <PlaylistCarousel playlists={playlists} />
+            </div>
+
+            <div className="w-3/4 mx-auto mt-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
+                    {songs.map((track: Track) => (
+                        <SongCard key={track.id} track={track} />
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
