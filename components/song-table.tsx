714bd10143ddace4bf8341332b705978c13d5fc2
Johnny Pillacela <johnnybelesaca@gmail.com>
2025-12-24 01:20:14 -0500

    handle missing album images

--- components/song-table.tsx
+++ components/song-table.tsx
@@ -4,6 +4,7 @@ import { Track } from "@spotify/web-api-ts-sdk";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
+import { Music } from "lucide-react";
 
 interface SongTableProps {
     tracks: Track[];
@@ -27,16 +28,26 @@ export function SongTable({ tracks }: SongTableProps) {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {tracks.map((track, index) => (
-                            <TableRow key={track.id}>
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell>
-                                    <Image src={track.album.images[0].url} alt={track.name} width={80} height={80} />
-                                </TableCell>
-                                <TableCell>{track.name}</TableCell>
-                                <TableCell>{track.artists.map((artist) => artist.name).join(", ")}</TableCell>
-                            </TableRow>
-                        ))}
+                        {tracks.map((track, index) => {
+                            const thumbnailUrl = track.album?.images?.[0]?.url;
+
+                            return (
+                                <TableRow key={track.id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>
+                                        {thumbnailUrl ? (
+                                            <Image src={thumbnailUrl} alt={track.name} width={80} height={80} />
+                                        ) : (
+                                            <div className="w-20 h-20 bg-muted flex items-center justify-center rounded">
+                                                <Music className="w-8 h-8 text-muted-foreground" />
+                                            </div>
+                                        )}
+                                    </TableCell>
+                                    <TableCell>{track.name}</TableCell>
+                                    <TableCell>{track.artists.map((artist) => artist.name).join(", ")}</TableCell>
+                                </TableRow>
+                            )
+                        })}
                     </TableBody>
                 </Table>
             </CardContent>
