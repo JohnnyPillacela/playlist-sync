a8ed72bdde036853cd0e9fa86e8180f6f14229b8
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-04 00:07:03 -0500

    Add AUTH_ERRORS constant to types.ts for standardized authentication error handling

--- lib/types.ts
+++ lib/types.ts
@@ -6,6 +6,17 @@
  * @template E - The type of the error.
  * @returns {Result<T, E>} A result object.
  */
-export type Result<T, E = string> = 
-  | { ok: true; data: T }
-  | { ok: false; error: E }
+export type Result<T, E = string> =
+    | { ok: true; data: T }
+    | { ok: false; error: E }
+
+/**
+ * A list of authentication errors.
+ * @returns {AUTH_ERRORS} A list of authentication errors.
+ */
+export const AUTH_ERRORS = {
+    SPOTIFY_NO_TOKEN_FOUND: 'spotify_no_access_token_found',
+    SPOTIFY_ACCESS_TOKEN_EXPIRED: 'spotify_access_token_expired',
+    GOOGLE_NO_TOKEN_FOUND: 'google_no_access_token_found',
+    GOOGLE_ACCESS_TOKEN_EXPIRED: 'google_access_token_expired',
+} as const;
