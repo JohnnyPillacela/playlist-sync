c1d91e4387ed06913e6cf8adfa5d699ae4e5e286
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-04 23:38:56 -0500

    Refactor page component to be server component instead of client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,27 @@
 // /app/page.tsx
 
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleSpotifyLogin = () => {
-    window.location.href = '/api/spotify/auth';
-  };
-
-  const handleGoogleLogin = () => {
-    window.location.href = '/api/youtube/auth';
-  }
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background">
       <h1 className="mb-4 text-4xl font-semibold">Welcome to the playlist sync app!</h1>
-      <Button variant="outline" size="lg" onClick={() => router.push('/dashboard')}>
-        View all playlists
-      </Button>
-      <Button variant="default" size="lg" onClick={handleSpotifyLogin}>
-       Login with Spotify
-      </Button>
-      <Button variant="default" size="lg" onClick={handleGoogleLogin}>
-        Login with Google
-      </Button>
+      <Link href="/dashboard">
+        <Button variant="outline" size="lg">
+          View all playlists
+        </Button>
+      </Link>
+      <Link href="/api/spotify/auth">
+        <Button variant="default" size="lg">
+          Login with Spotify
+        </Button>
+      </Link>
+      <Link href="/api/youtube/auth">
+        <Button variant="default" size="lg">
+          Login with Google
+        </Button>
+      </Link>
     </div>
   );
 }
