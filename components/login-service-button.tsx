b687162e6c2deb74b8ed5b61fc1d7faa35ac7cbc
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-05 01:18:40 -0500

    Update LoginServiceButton to use authUrl for redirection instead of onClick handler

--- components/login-service-button.tsx
+++ components/login-service-button.tsx
@@ -1,17 +1,20 @@
+// /components/login-service-button.tsx
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 interface LoginServiceButtonProps {
   serviceName: string;
-  onClick: () => void;
+  authUrl: string;
 }
 
 export function LoginServiceButton({
   serviceName,
-  onClick,
+  authUrl,
 }: LoginServiceButtonProps) {
   return (
     <Button
-      onClick={onClick}
+      onClick={() => window.location.href = authUrl}
       variant="outline"
       size="sm"
       className="text-blue-600 hover:text-blue-700 hover:bg-blue-50 border-blue-200"
