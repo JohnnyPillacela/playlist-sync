db37a6c7d8d8a408bf83f03e81bc9a2670737c90
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-06 16:23:36 -0500

    Add GET endpoint to retrieve YouTube user playlists.

--- app/api/youtube/playlists/route.ts
+++ app/api/youtube/playlists/route.ts
@@ -1,1 +1,20 @@
 // /api/youtube/playlists
+
+import { Result } from "@/lib/types";
+import { youtube_v3 } from "googleapis/build/src/apis/youtube/v3";
+import { NextRequest, NextResponse } from "next/server";
+import { getYoutubeUserPlaylists } from "@/lib/youtube/playlists";
+
+export async function GET(request: NextRequest): Promise<NextResponse> {
+    const youtubePlaylistsResult: Result<youtube_v3.Schema$Playlist[]> = await getYoutubeUserPlaylists();
+
+    if (!youtubePlaylistsResult.ok) {
+        return NextResponse.json(
+            { error: youtubePlaylistsResult.error },
+            { status: 500 }
+        );
+    }
+
+    const youtubePlaylists = youtubePlaylistsResult.data;
+    return NextResponse.json({ youtubePlaylists });
+}
