cae1b9cbb24b48ea4c5f40fbea8d5829ddb80bc2
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-11 16:37:24 -0500

    add data types for searching tracks

--- lib/youtube/search.ts
+++ lib/youtube/search.ts
@@ -1,1 +1,20 @@
 // lib/youtube/search
+
+interface YouTubeSearchOptions {
+    trackName: string;
+    artistNames: string[];
+    albumName?: string;
+    prioritizeAudio?: boolean; // default true
+}
+
+interface YouTubeSearchResult {
+    videoId: string;
+    title: string;
+    channelTitle: string;
+    confidence: number; // 0-1 score
+    isAudioOnly: boolean;
+}
+
+export async function searchTrack() {
+
+}
