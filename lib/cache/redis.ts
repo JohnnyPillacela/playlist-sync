3143f0595ab53eea18debcb7ca48489290fa3283
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-20 17:48:33 -0500

    Add timeout so Redis does not hang

--- lib/cache/redis.ts
+++ lib/cache/redis.ts
@@ -11,4 +11,6 @@ const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
 export const redis = new Redis({
   url: REDIS_URL,
   token: REDIS_TOKEN,
+  // Set timeout of 5 seconds for each request
+  signal: () => AbortSignal.timeout(5000)
 });
