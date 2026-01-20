2294f2ba838db31c6d64bbad305a38ae8680c88d
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-20 10:52:07 -0500

    Add Upstash Redis client for caching data

---
+++ lib/cache/redis.ts
@@ -0,0 +1,14 @@
+// lib/cache/redis.ts
+
+import { Redis } from "@upstash/redis";
+
+const REDIS_URL = process.env.UPSTASH_REDIS_REST_URL;
+const REDIS_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+/**
+ * Upstash Redis client for caching data.
+ */
+export const redis = new Redis({
+  url: REDIS_URL,
+  token: REDIS_TOKEN,
+});
