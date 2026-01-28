243c7124d7e0aecd76b38d755db56ceda97e38c5
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-01-28 13:44:37 -0500

    Add Google SDK integration for OAuth2 authentication

---
+++ lib/google/sdk.ts
@@ -0,0 +1,39 @@
+// /lib/google/sdk.ts
+
+import { cookies } from "next/headers";
+import { AUTH_ERRORS, Result } from "../types";
+import { GOOGLE_ACCESS_TOKEN_KEY } from "../constants/google";
+import { createOAuth2Client } from "../youtube/auth";
+import { google, oauth2_v2 } from "googleapis";
+
+const GOOGLE_SDK_INIT = "[GOOGLE_SDK_INIT]";
+
+export async function getGoogleSDK(): Promise<Result<oauth2_v2.Oauth2>> {
+    const cookieStore = await cookies();
+    const googleAccessToken = cookieStore.get(GOOGLE_ACCESS_TOKEN_KEY)?.value;
+    if (!googleAccessToken) {
+        console.error(`${GOOGLE_SDK_INIT} ${AUTH_ERRORS.GOOGLE_NO_TOKEN_FOUND}`);
+        return {
+            ok: false,
+            error: AUTH_ERRORS.GOOGLE_NO_TOKEN_FOUND,
+        }
+    }
+
+    // Create OAuth2Client and set credentials
+    const auth = createOAuth2Client();
+    auth.setCredentials({
+        access_token: googleAccessToken,
+    });
+
+    const googleSDK = google.oauth2({
+        version: 'v2',
+        auth,
+        rootUrl: 'https://www.googleapis.com',
+    });
+
+    // Return authenticated Google SDK instance
+    return {
+        ok: true,
+        data: googleSDK,
+    }
+}
