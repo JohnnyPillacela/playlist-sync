9ae6feafbd730263ea86a01025a44b85c91cdb58
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-02-11 11:11:47 -0500

    Add Spanish landing page component with localization support using getMessages function

---
+++ app/es/page.tsx
@@ -0,0 +1,9 @@
+// /app/es/page.tsx
+
+import { LandingPage } from '@/components/landing-page';
+import { getMessages } from '@/lib/i18n';
+
+export default async function EsHome() {
+  const messages = await getMessages('es', 'landing-page');
+  return <LandingPage messages={messages} />;
+}
