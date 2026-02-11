73ac82987236483ba3e0ee2f8e888dd7f1a5ab5f
Johnny Pillacela <johnnybelesaca@gmail.com>
2026-02-10 22:13:40 -0500

    Correct name

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center gap-4">
       <ThemeToggle className="absolute right-4 top-4" />
-      <h1 className="mb-4 text-4xl font-semibold">Waves Sync</h1>
+      <h1 className="mb-4 text-4xl font-semibold">Wave Sync</h1>
       <Link href="/dashboard">
         <Button variant="outline" size="lg">
           View all playlists
